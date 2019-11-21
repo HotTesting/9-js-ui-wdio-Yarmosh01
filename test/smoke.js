3b8bf1a97e0957c17e367d3badd59cbf88ff0ba6
Yarmosh01 <andrewyarm29@gmail.com>
2019-11-21 21:03:22 +0200

    Lection1Result
    just start

---
+++ test/smoke.js
@@ -0,0 +1,10 @@
+import { isMainThread } from "worker_threads"
+
+describe('Website', function(){
+    it('should be alive', function(){
+        browser.url('http://ip-5236.sunline.net.ua:38015/')
+        const img = 
+        const img = $('img[src=""]')
+
+    }
+})
