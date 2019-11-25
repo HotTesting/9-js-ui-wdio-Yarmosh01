8d4d322164dbce6a326d08b1c1601eb544903a08
Yarmosh01 <andrewyarm29@gmail.com>
2019-11-26 00:01:00 +0200

    ERR: No specs found to run

--- test/smoke.js
+++ test/smoke.js
@@ -1,10 +1,18 @@
 import { isMainThread } from "worker_threads"
 
-describe('Website', function(){
+/*describe('Website', function(){
     it('should be alive', function(){
         browser.url('http://ip-5236.sunline.net.ua:38015/')
         const img = 
         const img = $('img[src=""]')
 
     }
-})
+})*/
+describe('Website', function () {
+    it('should be alive', function () {
+    browser.url('/')
+    const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
+    assert(img.isExisting(), 'Website should be opened, and logo displayed')
+    browser.pause(5000)
+    })
+    })
