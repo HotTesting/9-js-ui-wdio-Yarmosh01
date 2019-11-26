48a6334664103bf1854cec2df2b6092fda3e355e
Yarmosh01 <yarmoshevich@es.lan>
2019-11-26 14:42:48 +0200

    Офигенный коммит

---
+++ test/specs/smoke.js
@@ -0,0 +1,13 @@
+//import { isMainThread } from "worker_threads"
+
+const assert = require('assert');
+
+describe('Website', function(){
+    it('should be alive', function(){
+        //browser.url('http://ip-5236.sunline.net.ua:38015/')
+        browser.url('/')
+const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
+assert(img.isExisting(), 'Website should be opened, and logo displayed')
+browser.pause(5000)
+})
+})
