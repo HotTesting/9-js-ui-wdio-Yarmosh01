89f491c5b8ca405aa3c224702978fc9edf08a91d
Yarmosh01 <andrewyarm29@gmail.com>
2019-11-29 00:20:55 +0200

    platform to work with selectors

---
+++ test/specs/registration.ts
@@ -0,0 +1,18 @@
+describe('Website', function () {
+    it('should be alive', function () {
+        browser.url('/create_account')
+        $('input[name = "firstname"]').setValue('Test')
+        $('input[name = "lastname"]').setValue('Test')
+
+        const countrySelect = $('select[name = "country_code"]')
+        countrySelect.selectByVisibleText('Ukraine')
+        
+        const email = 'test${new Date().getTime() / 1000}@test.com'
+
+        $('input[name = "email"]').setValue(email)
+        $('input[name = "phone"]').setValue('+380441111111')
+
+
+        
+    })
+    })
--- test/specs/smoke.ts
+++ test/specs/smoke.ts
@@ -2,13 +2,27 @@
 
 describe('Website', function () {
     it('should be alive', function () {
-        browser.url('http://ip-5236.sunline.net.ua:38015/')
+        browser.url('/')
+        // browser.url('http://ip-5236.sunline.net.ua:38015/')
         const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
         
        if(!img.isExisting()){
            throw new Error('Website should be opened');
        }
-       //assert(img.isExisting(), 'Website should be opened, and logo displayed')
-        browser.pause(5000)
+        browser.pause(500)
     })
+
+/*    it('test', function(){
+    browser.url('/')
+
+    const popularProducts = $$('#box-popular-products .product')
+
+    popularProducts[2].$('.name')
+
+    $('=Change').click()
+
+    $('=Purple Duck').$('../..') //2 levels up from Purple Duck
+    }
+    )
+    */
 })
